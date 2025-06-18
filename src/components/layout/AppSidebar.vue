cbf1cfada7e29bb019a1137c82864afbf9c4e0a0
dev2Cett <kaikebsilva62@gmail.com>
2025-06-18 15:22:01 -0300

    sem codigo no prod

--- src/components/layout/AppSidebar.vue
+++ src/components/layout/AppSidebar.vue
@@ -2,7 +2,7 @@
   <aside class="app-sidebar" :class="sidebarClasses">
     <div class="sidebar-header">
       <IconCube class="logo" :size="32" />
-      <span class="brand-name" v-if="!layoutStore.isSidebarCollapsed">Obras ADM teste</span>
+      <span class="brand-name" v-if="!layoutStore.isSidebarCollapsed">Obras ADM sem codigo</span>
       
       <!-- Botão para fechar o menu no mobile -->
       <button 
